2560df549ab196511682f80d805e42afcb3d3d6d
Nariman Ibadullayev <n.ibadullaev@inc4.net>
2023-02-24 12:05:53 +0400

    fix

--- main.go
+++ main.go
@@ -1,19 +1,55 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Users struct {
+	Id      int    `json:"id"`
+	Name    string `json:"name"`
+	Age     int    `json:"age"`
+	Address string `json:"address"`
+}
+
+var user Users = Users{
+	Id:      1,
+	Name:    "nariman",
+	Age:     35,
+	Address: "yasamal",
+}
+
+var users []Users
+
 func main() {
 
-	http.HandleFunc("/", myMiddleware(myHandler))
+	http.HandleFunc("/", loggingMiddleware(myMiddleware(usersHandler)))
+	http.HandleFunc("/user/:id", myMiddleware(userHandler))
 	http.ListenAndServe(":8082", nil)
 }
 
-func myHandler(w http.ResponseWriter, r *http.Request) {
-	log.Println("Hello from Handler")
-	w.WriteHeader(http.StatusOK)
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUsers(w, r)
+	case http.MethodPost:
+		addUser(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+}
+
+func userHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		getUser(w, r)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+	}
+
 }
 
 func myMiddleware(next http.HandlerFunc) http.HandlerFunc {
@@ -21,4 +57,33 @@ func myMiddleware(next http.HandlerFunc) http.HandlerFunc {
 		log.Println("Hello from Middleware")
 		next(w, r)
 	}
+}
+
+func loggingMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[%s] %s %s\n", r.Method, r.URL, r.RemoteAddr)
+		next(w, r)
+	}
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	log.Println("Hello from Handler")
+	w.WriteHeader(http.StatusOK)
+	resp, _ := json.Marshal(users)
+	w.Write(resp)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	// userId := r.Body
+	resp, _ := json.Marshal(users)
+	w.Write(resp)
+}
+
+func addUser(w http.ResponseWriter, r *http.Request) {
+	reqBytes, _ := ioutil.ReadAll(r.Body)
+	var newUser Users
+	json.Unmarshal(reqBytes, &newUser)
+	users = append(users, newUser)
+	fmt.Println(users)
 }
